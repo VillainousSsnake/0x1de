0609a9a3236b97145d777f3e66cb7259ea5e9ea0
VillainousSsnake <cjdiamond9@gmail.com>
2024-06-27 16:15:22 -0400

    Removed and did todo

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -3,6 +3,7 @@
 
 
 # Importing modules and libraries:
+from App.AppLib.updater import Updater
 import customtkinter as ctk
 
 
@@ -11,7 +12,7 @@ def main_menu(app):
 
     # Creating root window
     root = ctk.CTk()
-    root.title("0x1de - " + "(PLACEHOLDER:VERSION)" + " - Main Menu")  # TODO: Replace placeholders
+    root.title("0x1de - " + Updater.get_current_client_version() + " - Main Menu")
     root.geometry("850x525+200+200")
 
     # Defining on_close function
