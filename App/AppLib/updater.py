2782958ee5b339b9a27da88c9d829b9e839984c9
VillainousSsnake <cjdiamond9@gmail.com>
2024-06-27 16:11:17 -0400

    Added updater class

---
+++ App/AppLib/updater.py
@@ -0,0 +1,22 @@
+# /App/AppLib/updater.py
+# Contains Updater class
+
+# Importing dependencies
+
+# Updater class
+class Updater:
+    """
+    -----------------
+    | UPDATER CLASS |
+    -----------------
+
+    DESC:
+    --------------------------------------------------------------------------
+    |   Contains functions that do things with version and update stuff.     |
+    --------------------------------------------------------------------------
+    """
+
+    @staticmethod
+    def get_current_client_version():   # TODO: Placeholder
+        """Fetches the current file version"""
+        return "(PLACEHOLDER:VERSION)"
